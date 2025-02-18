32fdb09e8fa39b6feba816ec94d6ffb5411c3b5f
LUIS ARMANDO GOMEZ POMARI <lgomezp@unsa.edu.pe>
2025-02-18 12:44:36 -0500

    feat: add set sync data

--- lib/main.dart
+++ lib/main.dart
@@ -22,40 +22,65 @@ class PhoneScreen extends StatefulWidget {
 }
 
 class _PhoneScreenState extends State<PhoneScreen> {
-  final _wearConnectivity = FlutterWearOsConnectivity();
-  int _currentNumber = 0;
+  final wearConnectivity = FlutterWearOsConnectivity();
+  int currentNumber = 0;
 
   @override
   void initState() {
     super.initState();
-    _initialize();
+    init();
   }
 
-  Future<void> _initialize() async {
-    await _wearConnectivity.configureWearableAPI();
-    _startListening();
+  Future<void> init() async {
+    await wearConnectivity.configureWearableAPI();
+    startListening();
   }
 
-  void _startListening() {
-    _wearConnectivity
+  startListening() {
+    wearConnectivity
         .messageReceived(
       pathURI: Uri(scheme: 'wear', host: '*', path: '/randomNumber'),
     )
         .listen((message) {
       final numberStr = String.fromCharCodes(message.data);
       setState(() {
-        _currentNumber = int.parse(numberStr);
+        currentNumber = int.parse(numberStr);
       });
     });
+  }
+
+  setSyncData() async {
+    DataItem? dataItem = await wearConnectivity.syncData(
+        path: "/data-path",
+        data: {"message": "Data sync ${DateTime.now().millisecondsSinceEpoch}"},
+        isUrgent: true);
+    showToast('--- set sync data ${dataItem?.mapData}');
+  }
+
+  showToast(String label) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(label),
+      duration: const Duration(milliseconds: 300),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Phone App')),
-      body: Center(
-        child: Text('Número actual: $_currentNumber',
-            style: const TextStyle(fontSize: 24)),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Número actual: $currentNumber',
+                style: const TextStyle(fontSize: 24)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setSyncData();
+            },
+            child: const Text('Sincronizar'),
+          ),
+        ],
       ),
     );
   }
